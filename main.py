525f223557d62b4df7bb5f595b72e2ca2a4cdead
Conlan Rios <conlan@gmail.com>
2018-12-20 02:37:13 -0800

    Clean raw logs into human readable events

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def index():
 @app.route('/tasks/crawl')
 def crawl():
 	#TODO load this from datastore
-	mostRecentCrawledBlock = 6910037;
+	last_updated_block_number = 6910037;
 
 	# load the exchange contract ABI
 	EXCHANGE_ABI = open("static/exchangeABI.json", "r").read();
@@ -47,32 +47,59 @@ def crawl():
 
 	# collect up event topics
 	for event in exchange_contract.events._events:
+		# the event name
 		event_name = event["name"];
+		# the list of inputs and their types
 		event_inputs = event["inputs"];
 
-		# build up the event input that we'll Keccak-256 hash
-		event_input = [];
-		event_input.append(event_name);
-		event_input.append("(");
+		# build up the string that we'll Keccak-256 hash to find the topic hash for this event (ie "RemoveLiquidity(address,uint256,uint256)")
+		event_input_to_hash = [];
 
-		for input in event_inputs:
-			event_input.append(input["type"]);
-			event_input.append(",");
+		event_input_to_hash.append(event_name);
+		event_input_to_hash.append("(");
+
+		# store the data needed to decode log data
+		event_data = {
+			"event" : event_name,
+			"input_types" : [],
+			"input_names" : []
+		}
+
+		# for all the inputs
+		for input_data in event_inputs:
+			# get the type of the input (address, uint256)
+			event_input_type = input_data["type"];
+			# append to the event data's input type list
+			event_data["input_types"].append(event_input_type);
+
+			# get the name of the input parameter
+			event_input_name = input_data["name"];
+			# append to the event data's input name list
+			event_data["input_names"].append(event_input_name);
+
+			# append to the string that we'll be hashing (see above)
+			event_input_to_hash.append(event_input_type);
+			# append a comma
+			event_input_to_hash.append(",");
 
 		#delete last comma
-		del event_input[-1]
+		del event_input_to_hash[-1]
 
-		event_input.append(")");
+		# append trailing parentheses
+		event_input_to_hash.append(")");
 
-		# determine the topic hash (ie "RemoveLiquidity(address,uint256,uint256)")
-		event_input_txt = "".join(event_input);
+		# join all the strings to make the final string for hashing
+		event_input_txt = "".join(event_input_to_hash);
+		# determine the topic hash 
 		topic_hash = eth_utils_keccak(text=event_input_txt).hex();
 
-		topic_hashes[topic_hash] = event_name;
+		# associate the event data with its topic hash
+		topic_hashes[topic_hash] = event_data;
 
+	# grab all the contract logs for this exchange (since the last updated crawled block)
 	logs = web3.eth.getLogs(
 	    {
-     	   	"fromBlock": mostRecentCrawledBlock,
+     	   	"fromBlock": last_updated_block_number,
         	"toBlock": "latest",
         	"address": [
             	exchange_address
@@ -82,9 +109,35 @@ def crawl():
 
 	for log in logs:
 		log_topics = log["topics"];
+
 		topic_hash = remove_0x_prefix(log_topics[0].hex());
 
-		event_type = topic_hashes[topic_hash];
+		event = topic_hashes[topic_hash];
+
+		if (event["event"] == "Transfer"):
+			continue;
+
+		event_clean = {
+			"event" : event["event"],
+			"tx_hash" : log["transactionHash"].hex()
+		}
+
+		for i in range(1, len(log_topics)):
+			topic = log_topics[i];
+
+			# remove padding
+			topic = topic.hex().replace("0x000000000000000000000000", "0x");
+			
+			input_type = event["input_types"][i - 1];
+			input_name = event["input_names"][i - 1];
+
+			if (input_type == 'address'):
+				event_clean[input_name] = topic;
+			elif (input_type == 'uint256'):
+				value = web3.toInt(hexstr=topic);
+				event_clean[input_name] = value;
+
+		print(event_clean);
 
 	return "{todo}";
 
